42d1306bb0ba5f995fd07b59a9aa8292a882ab7b
chaikovskyit <chaikovskyit@gmail.com>
2020-11-05 12:12:10 +0200

    refactoring code, change ItemStatusFilter and ToDoListItem to class component

--- src/Components/ToDoListItem/ToDoListItem.jsx
+++ src/Components/ToDoListItem/ToDoListItem.jsx
@@ -1,35 +1,46 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './ToDoListItem.css'
 
-const ToDoListItem = ({label, important = false}) => {
-  const style = {
-    color: important ? "green" : "black",
-    fontWeight: important ? 'bold' : 'normal' 
-  }
-  return(
-    <span className="ToDoListItem">
-      <span
-        className="ToDoListItemLabel"
-        style={style}
-      >
-        {label}
+class ToDoListItem extends Component {
+
+  render() {
+    
+    const {label, important = false} = this.props
+
+    const style = {
+      color: important ? "green" : "black",
+      fontWeight: important ? 'bold' : 'normal' 
+    }
+
+    return(
+      <span className="ToDoListItem">
+        <span
+          className="ToDoListItemLabel"
+          style={style}
+        >
+          {label}
+        </span>
+  
+        <button 
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation" />
+        </button>
+  
+        <button 
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button 
-        type="button"
-        className="btn btn-outline-success btn-sm float-right"
-      >
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button 
-        type="button"
-        className="btn btn-outline-danger btn-sm float-right"
-      >
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  )
+    )
+  }
 }
+
+
+  
+  
 
 export default ToDoListItem
